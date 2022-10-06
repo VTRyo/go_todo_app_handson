ed08fa035b8f141c5e2d7d595593f20ae49ccfd2
VTRyo <vtryo.work@gmail.com>
2022-10-06 21:20:18 +0900

    list 16.16, mux_test.go

--- mux.go
+++ mux.go
@@ -6,7 +6,7 @@ import "net/http"
 // 内部実装に依存しない関数シグネチャになる
 func NewMux() http.Handler {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/health", func(writer http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json; charset=utf-8")
 		// 静的解析のエラーを回避するため明示的に戻り値を捨てる
 		_, _ = w.Write([]byte(`{"status": "ok"}`))
