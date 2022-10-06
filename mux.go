e77b41f8cc67f8b2af59900fd12690f4ad02a6b5
VTRyo <vtryo.work@gmail.com>
2022-10-06 21:12:08 +0900

    list 16.15, mux.go

---
+++ mux.go
@@ -0,0 +1,15 @@
+package main
+
+import "net/http"
+
+// 戻り値を *http.ServeMux型の値ではなく http.Handlerインターフェイスにしておくことで
+// 内部実装に依存しない関数シグネチャになる
+func NewMux() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/health", func(writer http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		// 静的解析のエラーを回避するため明示的に戻り値を捨てる
+		_, _ = w.Write([]byte(`{"status": "ok"}`))
+	})
+	return mux
+}
