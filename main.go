dac6ce143cb918d9181e96379e20e993fdf986fe
VTRyo <vtryo.work@gmail.com>
2022-10-09 23:19:53 +0900

    list 16.17 main.go refactor

--- main.go
+++ main.go
@@ -3,16 +3,13 @@ package main
 import (
 	"context"
 	"fmt"
+	"github.com/VTRyo/go_todo_app_handson/config"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
-	"time"
-
-	"github.com/VTRyo/go_todo_app_handson/config"
-	"golang.org/x/sync/errgroup"
 )
 
 //func main() {
@@ -64,32 +61,7 @@ func run(ctx context.Context) error {
 	url := fmt.Sprintf("http://%s", l.Addr().String())
 	log.Printf("start with: %v", url)
 
-	// *http.Server.ListenAndServeメソッドを実行してHTTPリクエストを受け付ける
-	// 引数で渡されたcontext.Contextを通じて処理の中段命令を検知したとき、*http.Server.ShutdownメソッドでHTTPサーバの機能を終了する
-	// run関数の戻り値として*http.Server.ListenAndServeメソッドの戻り値のエラーを返す
-	s := &http.Server{
-		// 引数で受け取ったnet.listenerを利用するので、Addrフィールドは指定しない
-		Handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			// コマンドラインで実験する用
-			time.Sleep(5 * time.Second)
-			fmt.Fprintf(w, "Hello, %s!", r.URL.Path[1:])
-		}),
-	}
-	eg, ctx := errgroup.WithContext(ctx)
-	// 別ゴルーチンでHTTPサーバを起動する
-	eg.Go(func() error {
-		// ListenAndServeメソッドではなくServeメソッドに変更する
-		if err := s.Serve(l); err != nil &&
-			err != http.ErrServerClosed {
-			log.Printf("failed to close: %+v", err)
-			return err
-		}
-		return nil
-	})
-
-	<-ctx.Done()
-	if err := s.Shutdown(context.Background()); err != nil {
-		log.Printf("failed to shutdown: %+v", err)
-	}
-	return eg.Wait()
+	mux := NewMux()
+	s := NewServer(l, mux)
+	return s.Run(ctx)
 }
